60074aca55284b8e6407e4d00b40a9bedbac84a0
Zac <zac.serocki@gmail.com>
2026-04-21 15:27:14 -0400

    generated data

--- env_install.sh
+++ env_install.sh
@@ -1,4 +1,6 @@
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
-pip install transformers datasets
+# pip install transformers datasets
+pip install transformers==4.30.0 datasets==2.12.0
+pip install "numpy>=1.26.0,<2.0.0" "pandas<2.0"
 pip install jsons appdirs blobfile cached-property httpx typer whoosh more_itertools jupyter openai
-pip install --upgrade protobuf==3.20.0
+pip install --upgrade protobuf==3.20.3
